e9f24bde3e38ebf6d95db4f9ac798e35c99c4b2f
root <pan.jiachun@outlook.com>
2026-01-14 22:56:14 +0800

    dpg

--- eval_base.sh
+++ eval_base.sh
@@ -7,5 +7,4 @@
 CUDA_VISIBLE_DEVICES=0, \
 torchrun --nproc_per_node=1 generation/generate_multi_base.py \
     "./prompts/evaluation_metadata.jsonl" \
-    --scale 7 \
     --outdir "generated_base"
---
+++ eval_dpg.sh
@@ -0,0 +1,9 @@
+# python generation/generate_multi.py \
+#     "./prompts/evaluation_metadata.jsonl" \
+#     --outdir "generated"
+
+
+CUDA_VISIBLE_DEVICES=0, \
+torchrun --nproc_per_node=1 generation/generate_multi_dpg.py \
+    "./dpg_prompts" \
+    --outdir "generated_dpg"
