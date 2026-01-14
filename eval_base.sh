fc8760c7e3ff6d12aba75ed402ba1e11bc8c6cad
jiachunp <134594875+jiachunp@users.noreply.github.com>
2026-01-14 21:56:06 +0800

    Update eval_base.sh

--- eval_base.sh
+++ eval_base.sh
@@ -7,4 +7,5 @@
 CUDA_VISIBLE_DEVICES=0, \
 torchrun --nproc_per_node=1 generation/generate_multi_base.py \
     "./prompts/evaluation_metadata.jsonl" \
+    --scale 7 \
     --outdir "generated_base"
